c28564dea974beb7ffc99b3e9c0b0acd41c0a000
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-07-04 18:19:58 +0530

    Update DuplicateNumber.cpp

--- Arrays/DuplicateNumber.cpp
+++ Arrays/DuplicateNumber.cpp
@@ -1,3 +1,7 @@
+// Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+// Cycle Detetction Approach, same as in Linked List.
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
