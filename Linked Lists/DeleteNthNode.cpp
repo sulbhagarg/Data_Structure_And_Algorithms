3eb8785fe2a6e6ffbe10e3bc3d34947e8798888b
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-07-05 22:40:18 +0530

    Create DeleteNthNode.cpp

---
+++ Linked Lists/DeleteNthNode.cpp
@@ -0,0 +1,39 @@
+// Given a linked list, remove the n-th node from the end of list and return its head.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int len=0;
+        ListNode* temp=head;
+        while(temp){
+            len++;
+            temp=temp->next;
+        }
+        if(n>len)
+            return head;
+        if(n==len){
+            head=head->next;
+            return head;
+        }
+        temp =head;
+        while(len-n!=1){
+            temp=temp->next;
+            len--;
+        }
+        if(temp->next)
+            temp->next = temp->next->next;
+        else
+            temp->next=NULL;
+        return head;
+    }
+};
