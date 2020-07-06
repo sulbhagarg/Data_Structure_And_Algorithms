b766629522f82ff8af576e482027020bf3275abe
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-07-06 16:33:04 +0530

    Create DetectCycle.cpp

---
+++ Linked Lists/DetectCycle.cpp
@@ -0,0 +1,25 @@
+/* To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. 
+If pos is -1, then there is no cycle in the linked list. */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
+};
