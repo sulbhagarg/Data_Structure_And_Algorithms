879f3327677140d2ac924a5110933b10048615dd
sulbhagarg <43903470+sulbhagarg@users.noreply.github.com>
2020-07-07 09:53:50 +0530

    Create AssignCookies.cpp

---
+++ Extras/AssignCookies.cpp
@@ -0,0 +1,20 @@
+/* Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie. Each child i has a greed factor gi, which is the minimum size of a cookie that the child will be content with; and each cookie j has a size sj. If sj >= gi, we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+
+Note:
+You may assume the greed factor is always positive.
+You cannot assign more than one cookie to one child. */
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i=0,j=0;
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i])
+                i++;
+            j++;
+        }
+        return i;
+    }
+};
